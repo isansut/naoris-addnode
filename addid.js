c3b47843c01c8ecc81e02c7f9b5ec4ccdb64d789
M Ihsan Fadilah <o.sanzyourbae@gmail.com>
2025-03-06 15:03:32 +0700

    Update addid.js

--- addid.js
+++ addid.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const axios = require('axios');
+const fs = require('fs');
 
 const API_URL = 'https://naorisprotocol.network/sec-api/api/switch';
 const BEARER_TOKEN = process.env.BEARER_TOKEN;
 const WALLET_ADDRESS = process.env.WALLET_ADDRESS;
+const DEVICE_FILE = 'devices.txt';
 
 if (!BEARER_TOKEN || !WALLET_ADDRESS) {
     console.error("BEARER_TOKEN atau WALLET_ADDRESS tidak ditemukan di .env!");
@@ -14,12 +16,19 @@ function getRandomDeviceHash() {
     return Math.floor(Math.random() * 10000000);
 }
 
+function saveDeviceHash(deviceHash) {
+    fs.appendFileSync(DEVICE_FILE, `${deviceHash}\n`, 'utf8');
+}
+
 async function switchState(state = 'ON') {
     try {
+        const deviceHash = getRandomDeviceHash();
+        saveDeviceHash(deviceHash);
+
         const response = await axios.post(API_URL, {
             walletAddress: WALLET_ADDRESS,
             state: state,
-            deviceHash: getRandomDeviceHash()
+            deviceHash: deviceHash
         }, {
             headers: {
                 'Authorization': `Bearer ${BEARER_TOKEN}`,
@@ -28,13 +37,14 @@ async function switchState(state = 'ON') {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
             }
         });
+
         console.log(`[${new Date().toISOString()}] Response:`, response.data);
     } catch (error) {
         console.error(`[${new Date().toISOString()}] Error:`, error.response ? error.response.data : error.message);
     }
 }
 
-async function startLoop(interval = 1000) { 
+async function startLoop(interval = 1000) {
     while (true) {
         await switchState();
         await new Promise(resolve => setTimeout(resolve, interval));
